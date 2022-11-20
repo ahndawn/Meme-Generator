898e994a8a860c4b37ab0ba1adff1972b79d0a92
ahndawn <115118638+ahndawn@users.noreply.github.com>
2022-11-20 09:44:45 -0500

    MemeGenerator

---
+++ meme.js
@@ -0,0 +1,85 @@
+const h1 = document.querySelector('h1');
+
+function randomRGB() {
+    //use rgb and create variables with the 'random' formula
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+
+    return `rgb(${r},${g},${b})`
+}
+
+setInterval(function(){
+    h1.style.color = randomRGB();
+}, 800);
+
+
+setInterval(function () { 
+    h1.classList.toggle('big')
+    h1.classList.toggle('small')
+    }, 1000)
+
+
+const imageFileInput = document.querySelector("#imageFileInput");
+const canvas = document.querySelector("#meme");
+const topTextInput = document.querySelector("#topTextInput");
+const bottomTextInput = document.querySelector("#bottomTextInput");
+
+let image;
+
+imageFileInput.addEventListener("change", () => {
+  const imageDataUrl = URL.createObjectURL(imageFileInput.files[0]);
+
+  image = new Image();
+  image.src = imageDataUrl;
+
+  image.addEventListener("load", () => {
+      updateMemeCanvas(
+        canvas,
+        image,
+        topTextInput.value,
+        bottomTextInput.value);
+    }, { once: true});
+});
+
+topTextInput.addEventListener("change", () => {
+  updateMemeCanvas(canvas, image, topTextInput.value, bottomTextInput.value);
+});
+
+bottomTextInput.addEventListener("change", () => {
+  updateMemeCanvas(canvas, image, topTextInput.value, bottomTextInput.value);
+});
+
+function updateMemeCanvas(canvas, image, topText, bottomText) {
+  const ctx = canvas.getContext("2d");
+  const width = image.width;
+  const height = image.height;
+  const fontSize = Math.floor(width / 10);
+  const yOffset = height / 25;
+
+  // Update canvas background
+  canvas.width = width;
+  canvas.height = height;
+  ctx.drawImage(image, 0, 0);
+
+  // Prepare text
+  ctx.strokeStyle = "black";
+  ctx.lineWidth = Math.floor(fontSize / 2);
+  ctx.fillStyle = "white";
+  ctx.textAlign = "center";
+  ctx.lineJoin = "round";
+  ctx.font = `${fontSize}px sans-serif`;
+
+  // Add top text
+  ctx.textBaseline = "top";
+  ctx.strokeText(topText, width / 2, yOffset);
+  ctx.fillText(topText, width / 2, yOffset);
+
+  // Add bottom text
+  ctx.textBaseline = "bottom";
+  ctx.strokeText(bottomText, width / 2, height - yOffset);
+  ctx.fillText(bottomText, width / 2, height - yOffset);
+}
+const submit = document.querySelector("submitBttn");
+
+submit.addEventListener("click", updateMemeCanvas);
